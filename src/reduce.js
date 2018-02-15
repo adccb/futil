e6ee651cae036156814ee50e83bb85c3c9ada668
autumn crossan <the@autumn.zone>
2018-02-15 15:04:15 -0500

    oops

--- src/reduce.js
+++ src/reduce.js
@@ -2,10 +2,8 @@
 
 import type { Either } from './types.js'
 
-const reduce = <T>(f: Function, b: T, a: Array<T>): Either<T, Function> =>
+export const reduce = <T>(f: Function, b: T, a: Array<T>): Either<T, Function> =>
   typeof a === 'undefined'
     ? (a: Array<any>) => reduce(f, b, a)
     : a.reduce(f, b)
-
-module.exports = { reduce }
 
