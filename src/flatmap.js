80ceccf4664f273cb6c4ad1b64837e4a7baebb05
autumn crossan <the@autumn.zone>
2018-03-10 10:29:37 -0500

    fixed typesystem oops

--- src/flatmap.js
+++ src/flatmap.js
@@ -7,6 +7,6 @@ import type { Either, NestedArray } from './types.js'
 
 export const flatMap = <T>(f: Function, a: NestedArray<T>): Either<Array<T>, Function> =>
   typeof a === 'undefined'
-    ? (a: NestedArray<any>) => ((map(f, flatten(a)): any): Array<T>)
+    ? (a: NestedArray<T>) => ((map(f, flatten(a)): any): Array<T>)
     : ((map(f, flatten(a)): any): Array<T>)
 
