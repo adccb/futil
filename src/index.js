ac49736c849357507dfd110cf661acdf74de0d7f
autumn crossan <the@autumn.zone>
2018-03-10 11:00:09 -0500

    aliased smoosh & smooshMap, why god

--- src/index.js
+++ src/index.js
@@ -10,5 +10,22 @@ import { reduce } from './reduce'
 import { lift } from './lift'
 import { attachProperty, lhs, range, rhs } from './func' 
 
-module.exports = { equals, unique, attachProperty, sum, every, filter, flatten, flatMap, lhs, map, range, reduce, rhs, lift }
+module.exports = { 
+  attachProperty, 
+  equals, 
+  every, 
+  filter, 
+  flatMap, 
+  flatten, 
+  lhs, 
+  lift 
+  map, 
+  range, 
+  reduce, 
+  rhs, 
+  smoosh: flatten,
+  smooshMap: flatMap,
+  sum, 
+  unique, 
+}
 
